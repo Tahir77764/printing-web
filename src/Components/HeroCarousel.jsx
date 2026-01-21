41f1b5ece40b368d8555a25d68bc308cd91b9a1d
Tahir77764 <tahirbaba907199@gmail.com>
2026-01-21 23:51:12 +0530

    resolving problem

--- src/Components/HeroCarousel.jsx
+++ src/Components/HeroCarousel.jsx
@@ -16,7 +16,45 @@ const HeroCarousel = () => {
       pagination={{ clickable: true }}
       className="w-full h-screen [&_.swiper-pagination]:hidden"
     >
+      {/* Slide 1 */}
+      <SwiperSlide>
+        <section className="relative h-[80vh] w-full flex items-center justify-center bg-HeroImgSmall1">
       
+      <div className="absolute inset-0 bg-black/30 backdrop-blur-[1px]"></div>
+
+      
+
+      <div className="relative flex flex-col lg:flex-row items-center w-full min-h-screen overflow-hidden pt-[95px] sm:pt-[10px] px-6">
+
+   {/* Left Content */}
+   <div className="flex flex-col items-start text-start w-full lg:w-[60%]">
+
+     <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-stroke HeroHeading text-[#205CCB] font-bold leading-tight">
+       Custom Tees & Caps That Elevate Your Brand
+     </h1>
+
+     {/* Description */}
+     <p className="mt-5 max-w-2xl text-base sm:text-lg text-white w-[80%]">
+       Premium custom apparel crafted to enhance your brand presence, style, and impact with lasting impressions.
+     </p>
+
+     {/* Buttons */}
+     <div className="mt-8 flex flex-col sm:flex-row gap-4">
+       <button className="px-8 py-3 bg-[#205CCB] text-white font-semibold rounded-full shadow-md hover:bg-[#2969e0] transition duration-300">
+         Shop Collection
+       </button>
+       <button className="px-8 py-3 bg-white text-[#205CCB] font-semibold rounded-full border-2 border-[#205CCB] shadow-md hover:bg-[#2969e0] hover:text-white transition duration-300">
+         Request a Quote
+       </button>
+     </div>
+
+   </div>
+
+
+      </div>
+    </section>
+
+      </SwiperSlide>
 
       {/* Slide 2 */}
       <SwiperSlide>
